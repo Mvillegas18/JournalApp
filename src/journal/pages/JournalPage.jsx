5c4f4df8a3e503dc0dd39426471452a14782e18a
Miguel Angel Villegas <chumilo227@gmail.com>
2023-07-31 16:37:46 -0500

    add componenet image gallery and floating button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,6 @@
+import { AddOutlined } from '@mui/icons-material'
+import { IconButton } from '@mui/material'
+
 import { JournalLayout } from '../layout/JournalLayout'
 import { NothingSelectedView } from '../views'
 
@@ -6,8 +9,14 @@ export const JournalPage = () => {
     <JournalLayout>
 
       <NothingSelectedView />
-      {/* Nothing selected */}
-      {/* Note View */}
+      {/* <NoteView /> */}
+
+      <IconButton
+        size='large'
+        sx={{ color: 'white', backgroundColor: 'error.main', ':hover': { backgroundColor: 'error.main', opacity: 0.9 }, position: 'fixed', right: 50, bottom: 50 }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   )
 }
