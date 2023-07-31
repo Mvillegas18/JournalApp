6129454311c4ac9bce41ef30d4edf3fab96f172d
Miguel Angel Villegas <chumilo227@gmail.com>
2023-07-31 13:19:32 -0500

    add page not selected

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,11 @@
-import { Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
+import { NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque blanditiis id nisi eos quisquam officia? Accusamus provident dolor, ipsam aliquam voluptatibus corporis dignissimos adipisci sint quos? Iste cumque eligendi nesciunt!</Typography>
 
+      <NothingSelectedView />
       {/* Nothing selected */}
       {/* Note View */}
     </JournalLayout>
